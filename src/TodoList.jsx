6420bde39dfa42dc2190331c775070e9d8e5c358
Sai Smruti Ranjan Das <160756794+saismrutiranjan18@users.noreply.github.com>
2024-10-23 20:18:37 +0530

    ToDo List

---
+++ src/TodoList.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+export default function TodoList() {
+    let [todos, setTodos] = useState([{task: "sample task", id: uuidv4()}]);
+    let [newTodo, setNewTodo] = useState("");
+
+    let addNewTask = () => {
+        setTodos( (prevTodos) => {
+            return [...prevTodos, {task: newTodo, id: uuidv4()}];
+        });
+        setNewTodo("");
+    }
+
+    let updateTodoValue = (event) => {
+        setNewTodo(event.target.value);
+    }
+
+    let deleteTodo = (id) => {
+        setTodos ((prevTodos) => todos.filter((prevTodos) => prevTodos.id != id));
+    }
+
+    let upperCaseAll = () => {
+        setTodos((prevTodos) => (
+            prevTodos.map((todo) => {
+                return {
+                    ...todo, 
+                    task: todo.task.toUpperCase(),
+                };
+            })
+        ));
+    };
+
+    let UpperCaseOne = (id) => {
+        setTodos((prevTodos) => 
+            prevTodos.map((todo) => {
+                if(todo.id == id){
+                    return {
+                        ...todo, 
+                        task: todo.task.toUpperCase(),
+                    };
+                } else{
+                    return todo;
+                }
+            })
+        );
+    }; 
+
+
+    return(
+        <div>
+            <input placeholder="add a task" value={newTodo} onChange={updateTodoValue}></input>
+            <br></br>
+            <br></br>
+            <button onClick={addNewTask}>Add Task</button>
+            <br></br>
+            <br></br>
+            <br></br>
+
+            <hr></hr>
+            <h4>Tasks Todo</h4>
+            <ul>
+                {
+                    todos.map((todo) => (
+                        <li key={todo.id}>
+                            <span>{todo.task}</span>
+                            &nbsp; &nbsp; &nbsp; &nbsp;
+                            <button onClick={ ()=> deleteTodo(todo.id)}>delete </button>
+                            <button onClick={ ()=> UpperCaseOne(todo.id)}>UpperCase One</button>
+                        </li>
+                    ))
+                }
+            </ul>
+            <br></br>
+            <button onClick={upperCaseAll}>UpperCase All</button>
+        </div>
+    );
+}
